02e0bdc82aa1439f9c56de946fde901235b932e5
Camaltra <mickael.boillaud@gmail.com>
2022-03-23 12:17:51 +0000

    Delete 14 stuff

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -38,13 +38,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int heightLeft, heightRight;
-
 	if (!tree)
 		return (0);
-
-	heightLeft = binary_tree_height(tree->left);
-	heightRight = binary_tree_height(tree->right);
 
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
