95c7833c893364b95766bb979474c37246e55f38
Camaltra <mickael.boillaud@gmail.com>
2022-03-23 12:26:01 +0000

    Adding task23

--- binary_trees.h
+++ binary_trees.h
@@ -54,4 +54,5 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 int binary_tree_is_complete_recursion(const binary_tree_t *tree, int idx, int nbNode);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 #endif
