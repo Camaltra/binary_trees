3d4e053fefbf33f3e801740e0374f73bf1127410
Camaltra <mickael.boillaud@gmail.com>
2022-03-23 14:31:32 +0000

    Modify functions name

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -12,7 +12,7 @@ size_t binary_tree_size_bis2(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (binary_tree_size_bis2(tree->left) + binary_tree_size_bis2(tree->right) + 1);
 }
 
 /**
